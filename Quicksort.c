2b197a11d911e8e930f43ccc79681c23894fa403
VikasNS <nsvikas1998@gmail.com>
2018-02-28 19:20:36 +0530

    Create Quicksort.c

---
+++ Quicksort.c
@@ -0,0 +1,71 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <time.h>
+ 
+int find_pivot(int arr[],int start,int end)
+{
+    
+    int pivot=start;
+    int l=start+1;
+    int r=end;
+    int temp;
+    while(l<=r)
+    {
+        if(arr[l]>=arr[pivot] && arr[r]<=arr[pivot])
+        {
+            temp=arr[l];
+            arr[l++]=arr[r];
+            arr[r--]=temp;
+        }else if(arr[l]>arr[pivot]) r--;
+        else l++;
+    }
+    temp=arr[pivot];
+    arr[pivot]=arr[r];
+    arr[r]=temp;
+    
+    return r;
+    
+}
+
+void quick_sort(int arr[],int start,int end)
+{
+    
+    if(start<end)
+    {
+        int pivot=find_pivot(arr,start,end);
+        quick_sort(arr,start,pivot-1);
+        quick_sort(arr,pivot+1,end);
+        
+    }
+}
+
+
+
+void main()
+{
+    clock_t start,end;
+    int arr[1000000];
+    for(int i=0;i<1000000;i++)
+    {
+        arr[i]=rand();
+    }
+    
+    start=clock();
+    quick_sort(arr,0,999999);
+    end=clock();
+    
+    printf("Total time taken is %lf",((double)(end-start))/CLOCKS_PER_SEC);
+    
+        
+    
+}
+
+
+
